d61495a2e0c2cde04f0bb83c8234f3ec92d6f8e5
saadeddinne <saadeddine.feki@gmail.com>
2020-11-19 13:16:49 -0500

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock "~> 3.14.1"
 before 'deploy', 'rvm1:install:ruby'
 set :rvm_map_bins, [ 'rake', 'gem', 'bundle', 'ruby', 'puma', 'pumactl' ]
 set :application, "SaadeddineFeki"
-set :repo_url, "git@github.com:saadeddinne/Rocket-Elevator-Foundation.git"
+set :repo_url, "git@github.com:saadeddinne/Rocket-Elevator-Foundation"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
