1a65b29b831168a87be7022a2d5370cc4ef91522
saadeddinne <saadeddine.feki@gmail.com>
2020-11-19 08:45:45 -0500

    fix twilio

--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -1,41 +1,69 @@
 class Elevator < ApplicationRecord
   belongs_to :column
-  # before_update :twilio_sms
+  before_update :twilio_sms
   # before_update :slack
 
 
 
-#   def twilio_sms
+  def twilio_sms
 
-#       #if the elevator status change to Intervention, you'll receive a text message 
+      #if the elevator status change to Intervention, you'll receive a text message 
 
-#     if self.elevator_status == "Intervention"
-#       account_sid = ENV["twilio_account_sid"]
-#       auth_token = ENV["twilio_auth_token"] 
+    if self.elevator_status == "Intervention"
+      account_sid = ENV["twilio_account_sid"]
+      auth_token = ENV["twilio_auth_token"] 
 
-#       @client = Twilio::REST::Client.new account_sid, auth_token
-#       message = @client.messages.create(
-#           body: "The elevator with Serial Number #{self.serial_number} require maintenance.",
-#           to: self.column.battery.building.technical_contact_phone,
-#           from: "+12058909021")  
+      @client = Twilio::REST::Client.new account_sid, auth_token
+      message = @client.messages.create(
+          body: "The elevator with Serial Number #{self.serial_number} require maintenance.",
+          # to: self.column.battery.building.technical_contact_phone,
+          to: "+15817772256",
+          from: "+15817772256")  
       
-#       puts message.sid
+      puts message.sid
+      
+    end
+  end
+
+#   def twilio_message
+#     if self.elevator_status == "Intervention"
+
+#       puts "************************"
+#       puts self.column.battery.building.technical_contact_phone
+#       puts self.column.battery.building.technical_contact_full_name
+#       puts self.id
+#       puts self.serial_number
+#       puts self.column.battery.building.address.number_and_street
+#       puts self.column.battery.building.address.city
+#       puts self.column.battery.building.administrator_full_name
+#       puts self.column.battery.building.administrator_phone_number
+#       puts "****** end ******"
+      
+#         account_sid = ENV["twilio_account_sid"]
+#         auth_token = ENV["twilio_auth_token"]
+#         @client = Twilio::REST::Client.new account_sid, auth_token 
+#         @client.api.account.messages.create(
+#         from: "+15817772256",
+#         to: self.column.battery.building.technical_contact_phone,
+#         body: "Hi #{self.column.battery.building.technical_contact_full_name}, the elevator: #{self.id} with Serial Number: #{self.serial_number}, located at #{self.column.battery.building.address.number_and_street}, #{self.column.battery.building.address.city} require intervention. Please contact the administrator #{self.column.battery.building.administrator_full_name} on the phone #{self.column.battery.building.administrator_phone_number}.")
+
       
 #     end
-#   end
-
-
-
-#   def slack
-#       if self.elevator_status_changed?
-#         require 'date'
-#         current_time = DateTime.now.strftime("%d-%m-%Y %H:%M")
-#         notifier = Slack::Notifier.new ENV["slack_api_url"]
-#         notifier.ping "The Elevator #{self.id} with Serial Number #{self.serial_number} changed status from #{self.elevator_status_was} to #{self.elevator_status} at #{current_time}."
+# end
   
 
-#       end
-#   end 
+
+
+  def slack
+      if self.elevator_status_changed?
+        require 'date'
+        current_time = DateTime.now.strftime("%d-%m-%Y %H:%M")
+        notifier = Slack::Notifier.new ENV["slack_api_url"]
+        notifier.ping "The Elevator #{self.id} with Serial Number #{self.   serial_number} changed status from #{self.elevator_status_was} to #{self.elevator_status} at #{current_time}."
+  
+
+      end
+  end 
   
   end
 
