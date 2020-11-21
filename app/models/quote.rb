d5d78018a78526f155ccd3f051314b5573919967
saadeddine <saadeddine.feki@gmail.com>
2020-11-20 20:27:25 -0500

    deploy

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,7 +1,7 @@
 class Quote < ApplicationRecord
   belongs_to :user, optional: true
-  after_create :new_quote_ticket
-  after_save :save_to_dwh
+  # after_create :new_quote_ticket
+  # after_save :save_to_dwh
 
 def save_to_dwh
  FactQuote.create!({quote_id: self.id})
