0b3065cdf74c99268d04795b4c7697299a063b9d
saadeddinne <saadeddine.feki@gmail.com>
2020-11-23 17:42:32 -0500

    first start with TDD

--- lib/ElevatorMedia/streamer.rb
+++ lib/ElevatorMedia/streamer.rb
@@ -1,5 +1,14 @@
 module ElevatorMedia
     class Streamer
+
+        # def initialize
+        #     @streamer = Streamer.new
+        # end
+        def new
+            @streamer = Streamer.new
+          end
+        def getContent 
+        end
         
     end
 end
--- spec/ElevatorMedia/streamer_spec.rb
+++ spec/ElevatorMedia/streamer_spec.rb
@@ -1,10 +1,28 @@
 require 'ElevatorMedia/streamer'
 
 
+    
 describe ElevatorMedia::Streamer do
 
-    it "should return true: First test" do
-        expect(true).to be true        
+    
+    before do
+        @streamer = ElevatorMedia::Streamer.new
+    end
+    
+    context "First Test" do
+        it "should return true: First test" do
+            expect(true).to be true        
+        end
+    end
+    context "generate streamer object" do
+        it "get new streamer object" do
+            expect(@streamer).to be_an_instance_of(ElevatorMedia::Streamer)    
+        end
+    end
+    context "getContent Test" do
+        it "getContent does she work ?" do
+            expect(@streamer).to respond_to(:getContent)        
+        end
     end
 end
 
