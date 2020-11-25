319574990dd4411b666d2b993a146a0a1a27b870
saadeddinne <saadeddine.feki@gmail.com>
2020-11-25 03:25:58 -0500

    Covid API

--- lib/elevator_media/streamer.rb
+++ lib/elevator_media/streamer.rb
@@ -1,20 +1,29 @@
 require 'open_weather'
-
-
+require "uri"
+require "net/http"
 puts "before app: #{ENV["OPEN_WEATHER_KEY"].inspect}"
+
 module ElevatorMedia
     class Streamer
         def new
             @streamer = Streamer.new
         end
         # version 1:: return first media content weather by city ID getContent(city) 
-        # Version2:: 
-        def getContent(city)         
-            weather =  self.getForecast(city)
-            if weather["cod"] == '404'
-                html = "ERROR 404"
-            else
-                html = "<html><body><div class='forecast'> <h3>City</h3>: #{weather["name"]}, #{weather["sys"]["country"]}, #{weather["weather"][0]["description"]}, #{weather["main"]["temp"]} °C, #{weather["main"]["humidity"]} Humidity  </div></body></html>"
+        # Version2:: get media content specified by argument: forecast or covid
+        def getContent(city, media) 
+           
+            if (media == "Covid") 
+                getCovidStats() 
+            end
+            if (media == "Forecast") 
+                weather =  self.getForecast(city)
+                if weather["cod"] == '404'
+                    html = "ERROR 404"
+                else
+                    html = "<html><body><div class='forecast'> <h3>City</h3>: #{weather["name"]}, #{weather["sys"]["country"]}, #{weather["weather"][0]["description"]}, #{weather["main"]["temp"]} °C, #{weather["main"]["humidity"]} Humidity  </div></body></html>"
+                end
+            else 
+                html = "Undefined media type !"
             end
             return html
         end
@@ -23,6 +32,10 @@ module ElevatorMedia
             options = { units: "metric", 
             APPID: ENV["OPEN_WEATHER_KEY"] }             
             OpenWeather::Current.city_id(city, options)
+        end 
+        # Return Stats about Covid-19 in a city (canada)
+        def getCovidStats 
+            return "yes Covid"
         end
         
     end
--- spec/lib/elevator_media/streamer_spec.rb
+++ spec/lib/elevator_media/streamer_spec.rb
@@ -5,6 +5,7 @@ require 'elevator_media/streamer.rb'
 describe ElevatorMedia::Streamer do    
     before do
         @streamer = ElevatorMedia::Streamer.new
+        @quebec = "6325494"
     end
     
     context "First Test" do
@@ -18,17 +19,17 @@ describe ElevatorMedia::Streamer do
             expect(@streamer).to be_an_instance_of(ElevatorMedia::Streamer)    
         end
     end
-    # Test the response of the method getContent and type
+    # Test the response of the method getContent 
     context "getContent Test" do
         it "getContent does she work ?" do
             expect(@streamer).to respond_to(:getContent)        
         end
     end
-    # Test first data contenu should be returned by getContent
-    # Test html returned by Forecast
+    # Test: getContent should return data
+    # Test: Forecast should return data
     context "should return html: forcast" do
         it "return valid HTML" do
-            html = @streamer.getContent("6325494")
+            html = @streamer.getContent(@quebec, "Forecast")
             puts html
             expect(html).to include('div')
             # expect(html).to have_tag('div')
@@ -37,12 +38,20 @@ describe ElevatorMedia::Streamer do
     # Test to verify if the returned API not 404
     context "what if getcontext return 'nil' with a wrong city id" do
         it "handle 404 result from API" do
-            html = @streamer.getContent("632549")
+            # Call with wrong city ID
+            html = @streamer.getContent("632549", "Forecast")
             puts html
             expect(html).to eq("ERROR 404")
             # expect(html).to have_tag('div')
         end
     end
-    
+    # Test the response of the method : getCovidStats
+    context "getCovidStats Test: Check the call of the method" do
+        it "Test the call of the method getCovidStats ?" do
+            expect(@streamer).to respond_to(:getCovidStats)  
+            @streamer.getContent("632549", "Covid")      
+        end
+    end
+   
 end
 
