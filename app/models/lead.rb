ab25ef7b3129b943d0f940f56a67c3259dad391e
saadeddinne <saadeddine.feki@gmail.com>
2020-12-24 23:33:27 -0500

    fix Zendesk

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -15,28 +15,28 @@ class Lead < ApplicationRecord
 
 
     # ZENDESK SECTION FOR THE CONTACT FORM 
-    # def new_lead_ticket
-    #     client = ZendeskAPI::Client.new do |config|
-    #       config.url = 'https://rocketelevators8100.zendesk.com/api/v2'
-    #       config.username = 'saadeddine.feki@gmail.com'
-    #       config.token = ENV["zendesk_api"]
-    #     end
+    def new_lead_ticket
+        client = ZendeskAPI::Client.new do |config|
+          config.url = 'https://rocketelevators8100.zendesk.com/api/v2'
+          config.username = 'saadeddine.feki@gmail.com'
+          config.token = ENV["zendesk_api"]
+        end
   
-    #     # This part is the configuration of zendesk api 
+        # This part is the configuration of zendesk api 
 
-    #     ZendeskAPI::Ticket.create!(client,
-    #     :subject => "#{self.full_name} from #{self.business_name}",
-    #     :comment => {
-    #       :value => "The contact #{self.full_name} from #{self.business_name} can be reached at email #{self.email} and at phone number #{self.phone}. \n #{self.department} has a project name #{self.project_name} which will require contribution from Rocket Elevators. \n Project Desciption : \n #{self.project_description} \n Attached message : \n #{self.message} \n The Contact uploaded an attachment #{self.attachment}" 
-    #     },
-    #     :requester => {
-    #       "name": self.full_name,
-    #       "email": self.email
-    #     },
-    #     # This is the priority for viewing tickets, if urgent, tickets will appear at the top of the list
-    #     :priority => "normal",
-    #     :type => "Question"
-    #     )
-    # end
+        ZendeskAPI::Ticket.create!(client,
+        :subject => "#{self.full_name} from #{self.business_name}",
+        :comment => {
+          :value => "The contact #{self.full_name} from #{self.business_name} can be reached at email #{self.email} and at phone number #{self.phone}. \n #{self.department} has a project name #{self.project_name} which will require contribution from Rocket Elevators. \n Project Desciption : \n #{self.project_description} \n Attached message : \n #{self.message} \n The Contact uploaded an attachment #{self.attachment}" 
+        },
+        :requester => {
+          "name": self.full_name,
+          "email": self.email
+        },
+        # This is the priority for viewing tickets, if urgent, tickets will appear at the top of the list
+        :priority => "normal",
+        :type => "Question"
+        )
+    end
 end
 
